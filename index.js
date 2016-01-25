0268c4d212adcc65c6620b17efe16fc8e0ee9389
Melissa Khuat <mkhuat@cs.washington.edu>
2016-01-24 23:47:04 -0800

    display HTML content

--- index.js
+++ index.js
@@ -1,31 +1,73 @@
-// Melissa Khuat, CSE 154 AC, Homework 7: ASCIImation
-// Creates and animates ASCII art with user's chosen animation, size, and speed.
+/* 
+ * Melissa Khuat
+ * Computer Science & Engineering
+ * University of Washington, 2017
+ * Slack Coding Exercise: Website Analyzer
+ * 
+ * High-level overview: Allows dynamic fetching of HTML source from a given URL.
+ *						
+ * 
+ */ 
 
 "use strict";
 
 (function() {
-	// var timer = null; // stores ID of interval timer ////////////////////////////// A BUNCH OF CONSTANT VARIABLES!!!
-	// var currentFrame = 0; // index of the frame the animation is on
 
 	// Sets up the event handlers on the page
 	window.onload = function() {
 		var queryButton = document.getElementById("query");
-		queryButton.onclick = start;
+		queryButton.onclick = getWebContent;
 		
 	};
 
-	function start() {
-		var name = document.getElementById("urlhi").value;
-		
-		var analysisArea = document.getElementById("myAnalysis");
+	// 
+	function getWebContent() {
+		var url = document.getElementById("url").value;
 		var sourceCodeArea = document.getElementById("mySource");
-		
-		analysisArea.innerHTML = name;
-		
+	
+		// Check for empty url strings
+		if (url.trim() == "") {
+			// make a div saying "Please enter a valid website address"
+			return;
+		}
+	
+		// The Ajax requests require "http://" to prefix the website name
+		// Example: request for "slack.com" must be prefixed with "http://"
+		if (url.indexOf("http://") != 0 && url.indexOf("https://") != 0) {
+			var prefix = "http://"
+			url = prefix.concat(url);
+		}
 			
-		// var stuff = document.createElement("p");
-		// stuff.innerHTML = name;
-		// analysisArea.appendChild(stuff);
+		$.ajax({
+			url: url,
+			type: 'GET',
+			dataType: 'xml',
+			success: function(data) {
+				// Display tag statistics
+				
+				var xmlDoc = data.responseXML;
+				var res = document.createElement('p');
+				res.innerHTML = xmlDoc.length;
+				sourceCodeArea.appendChild(res);
+ 				
+				// Display source code
+/* 				var source = data.responseText;
+				var res = document.createElement('xmp');
+				res.innerHTML = source;
+				res.classList.add("wrap-text");
+				sourceCodeArea.appendChild(res);
+ */			}
+		});
+	}
+	
+	// Sets display of element with given id (show or hide)
+	function toggleLoad() {
+		display = "";
+		var load = document.getElementById("loading");
+		if (load.style.display == "") display = "none";
+		load.style.display = display;
 	}
 	
 })();
+
+
